6b30e1481a19af0de5469ddf02ffbc493b2b35b0
ligianoazzi <ldaazzi@gmail.com>
2017-05-01 18:49:14 -0300

    Aulas 4 e 5 - Service Container

--- index.php
+++ index.php
@@ -3,14 +3,19 @@
 require_once "IConn.php";
 require_once "Conn.php";
 require_once "Product.php";
+require_once "Container.php";
 
 //$db = new \PDO("mysql:host=localhost;dbname=ligiano","root","root");
-$db = new Conn("mysql:host=localhost; dbname=ligiano", "root", "root");
+//$db = new Conn("mysql:host=localhost; dbname=ligiano", "root", "root");
 // a diferença é que não chamo mais direto o PDO, chamo a classe Conn
+$db = Container::getConn();
 
-$product = new Product($db);
+
+//$product = new Product($db);
 // a variavel $product armazena uma instancia da classe Product
 // ao instanciar enviou a variável $db com as configurações de conexão ao banco de dados 
+
+$product = Container::getProduct();
 
 
 $list = $product->lista();
