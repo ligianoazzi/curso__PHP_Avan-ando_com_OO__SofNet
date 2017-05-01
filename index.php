91ca860b93267f3ed99636c730b744ff0aa4a1ad
ligianoazzi <ldaazzi@gmail.com>
2017-05-01 09:30:03 -0300

    Aula 3 - Class Product

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 
 require_once "Product.php";
 
-$db = new \PDO("mysql:host=localhost;dbname=test","root","root");
+$db = new \PDO("mysql:host=localhost;dbname=ligiano","root","root");
 
 $product = new Product($db);
+// a variavel $product armazena uma instancia da classe Product
+// ao instanciar enviou a variável $db com as configurações de conexão ao banco de dados 
+
 
 $list = $product->lista();
+// $list chamou a function lista da classe Product
 
 var_dump($list);
+// var_dump() vai imprimir o array em tela
+
 
 ?>
