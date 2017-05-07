924e48da9a7a36c358057ee097fbe48434a979a1
ligianoazzi <ldaazzi@gmail.com>
2017-05-07 15:43:52 -0300

    Aula 6 - Refatorando o codigo

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+/*require_once "IConn.php";
+require_once "Conn.php";
+require_once "Product.php";
+require_once "Container.php";*/
+//troquei o codigo acima, pelo abaixo
+require_once "../vendor/autoload.php";
+
+
+//$db = new \PDO("mysql:host=localhost;dbname=ligiano","root","root");
+//$db = new Conn("mysql:host=localhost; dbname=ligiano", "root", "root");
+// a diferença é que não chamo mais direto o PDO, chamo a classe Conn
+
+//$db = Container::getConn();
+
+
+//$product = new Product($db);
+// a variavel $product armazena uma instancia da classe Product
+// ao instanciar enviou a variável $db com as configurações de conexão ao banco de dados 
+
+//$product = Container::getProduct();
+
+
+//$list = $product->lista();
+// $list chamou a function lista da classe Product
+
+//var_dump($list);
+// var_dump() vai imprimir o array em tela
+
+
+?>
--- readme.txt
+++ readme.txt
@@ -3,3 +3,7 @@ criar a interface - do metodo de conexao (o molde) IConn.php
 criar a classe    - do método de conexão (esta que realmente vai realizar a conexao) Conn.php
 
 O instrutor recomenda sempre programar para uma interface, ou seja, com molde pré-definido
+
+** Aula 6 - refatorando o aplicação **
+Configurou autoload
+assim cai fora o monte de require_once 
